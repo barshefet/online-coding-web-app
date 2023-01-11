8cb6c9de222904b90a5c19fa86ab92fe8aaa06d9
barshefet <bar.shefet@gmail.com>
2023-01-11 14:36:31 +0200

    initial general design implemented

--- src/client/src/Components/Lobby/Lobby.tsx
+++ src/client/src/Components/Lobby/Lobby.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import "./Lobby.scss";
 
 const Lobby = () => {
   return (
     <>
-    <div className="title">Choose code block</div>
-    <div className="code-block">1</div>
-    <div className="code-block">2</div>
-    <div className="code-block">3</div>
-    <div className="code-block">4</div>
+      <div className="lobby-container">
+        <div className="title">
+            <h1>Welcome,</h1>
+          <h1>Choose a Code Block</h1>
+        </div>
+        <div className="code-container">
+          <div className="code-block">1</div>
+          <div className="code-block">2</div>
+          <div className="code-block">3</div>
+          <div className="code-block">4</div>
+        </div>
+      </div>
     </>
-  )
+  );
 };
 
 export default Lobby;
