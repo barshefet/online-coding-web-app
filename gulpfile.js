8a8a1d914d28faacf88c055f094d0113e7008d69
barshefet <bar.shefet@gmail.com>
2023-01-13 10:29:05 +0200

    gulp reconfig for deploy

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ gulp.task('heroku-copy-non-client', () => {
     './package.json',
     './package-lock.json',
     './Procfile',
-    './dist/server/server.js',
+    './dist/server/*',
   ])
     .pipe(gulp.dest('./deploy'));
 });
